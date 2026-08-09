c0e9c1ec75515d2b3f3bd92d0efc1b12f5f1e5d9
Naik Alam <naikalamakhss0007@gmail.com>
2026-08-09 23:46:16 +0500

    Task02 Upload

--- Task02/Task02_Login_Registration_System.cpp
+++ Task02/Task02_Login_Registration_System.cpp
@@ -52,3 +52,83 @@ void UserRegister(){
     cout<<"Registration Successful!"<<endl;
 
 }
+
+//Function for User Login
+
+void UserLogin(){
+    string username;
+    string password;
+
+    cout<<"\n === Login Page ==="<<endl;
+
+    cout<<"Enter username: ";
+    cin>>username;
+    cout<<"Enter password: ";
+    cin>>password;
+
+    ifstream file("users.txt");
+
+    string storedUsername;
+    string storedPassword;
+
+
+    // Checking username and Password to login after validation
+
+    while (file >> storedUsername >> storedPassword){
+
+        if (storedUsername==username && storedPassword==password){
+            
+            file.close();
+
+            cout<<"  Login Successful! "<<endl;
+            cout<<"  Welcome "<<username<<" ! "<<endl;
+
+            return;
+        }
+    }
+
+    file.close();
+
+    cout<<"Error : Invalid username or password \n please try again ..."<<endl;
+
+}
+
+int main(){
+
+    //task 02 Login And Registration System
+    int choice;
+
+    do{
+        cout<<"\n === LOGIN AND REGISTRATION SYSTEM ==="<<endl;
+        cout<<"1. Register "<<endl;
+        cout<<"2. Login "<<endl;
+        cout<<"3. Exit "<<endl;
+
+        cout<<"Enter your Choice"<<endl;
+        cin>>choice;
+
+        switch (choice)
+        {
+        case 1:
+            //calling registration function
+            UserRegister();
+            break;
+        
+        case 2:
+            //calling Login Function
+            UserLogin();
+            break;
+
+        case 3:
+            cout<<"Exiting Program...."<<endl;
+            break;
+
+        default:
+            cout<<"Invalid Choice!"<<endl;
+            break;
+        }
+
+    }while(choice!=3);
+
+    return 0;
+}
